4c7683f061abbe7f3dd42790ca5d2cac20d0d6dc
freakfan15 <vivekkrrai.bxr@gmail.com>
2021-10-22 17:34:16 +0530

    min add abs difference b/w in a cicular array

---
+++ Arrays/min adjacent diff in circular array.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    //Function to find minimum adjacent difference in a circular array.
+    // arr[]: input array
+    // n: size of array
+    int minAdjDiff(int arr[], int n){    
+        // Your code here
+        int minA = abs(arr[0]-arr[1]);
+        for(int i = 1; i<n-1; i++){
+            minA = min((abs(arr[i]-arr[i+1])), minA);
+        }
+        int minB = abs(arr[n-1] - arr[0]);
+        return min(minA, minB);
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; //Input testcases
+    
+    while(t--) //While testcases exist
+    {
+        int n;
+        cin>>n; //Input size of array
+        
+        int arr[n]; //Array of size n
+        
+        for(int i = 0; i < n; i++)
+        {
+            cin>>arr[i]; //input elements of array
+        }
+        Solution ob;
+        
+        cout << ob.minAdjDiff(arr, n) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
